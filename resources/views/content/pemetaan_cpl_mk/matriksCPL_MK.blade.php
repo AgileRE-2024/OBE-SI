6ec5f0eeb3da58278cd81812d6c3aa1d9ae92cf7
arumtiyash <arum.tiyas.handayani-2020@fst.unair.ac.id>
2023-05-03 21:28:05 +0700

    update

--- resources/views/content/pemetaan_cpl_mk/matriksCPL_MK.blade.php
+++ resources/views/content/pemetaan_cpl_mk/matriksCPL_MK.blade.php
@@ -5,7 +5,7 @@
         <div class="card border" style="background-color: white">
             <div class="card-body" style="font-weight:600;">
                 <h3>Matriks Capaian Pembelajaran Lulusan (CPL) & Mata Kuliah (MK)</h3>
-                <h5 style="font-weight: 400;"><b><i class="bi bi-quote"></i></b>
+                <h5 style="font-weight: 400;"><b><i class="bi bi-quote"></i></b>Pemetaan CPL Program Studi terhadap PL
                     Pemetaan CPL terhadap MK dilakukan untuk menunjukkan keterhubungan antara
                     mata kuliah terhadap CPL Program Studi.<b style="display:inline-block;transform: scaleX(-1)"><i
                             class="bi bi-quote"></i></b></h5>
@@ -13,7 +13,7 @@
         </div>
         <div class="d-flex justify-content-end pt-2">
             <div class="pr-3">
-                <a class="btn btn-outline-danger" href="{{ route ('kurikulum.pemetaan.cetakpdfcplmk') }}"><i class="bi bi-file-earmark-pdf-fill"> </i>Export PDF</a>
+                <a class="btn btn-outline-danger" href="{{ route('kurikulum.pemetaan.cetakpdfcplmk') }}"><i class="bi bi-file-earmark-pdf-fill"> </i>Export PDF</a>
             </div>
             <div>
                 <a class="btn btn-success" href="{{ route('kurikulum.pemetaan.cetakexcelcplmk') }}"><i class="bi bi-file-earmark-excel"> </i>Export Excel</a>
@@ -50,27 +50,13 @@
                             {{ $mk->kodeMK }}</th>
                         <th scope="row" class="text-start">
                             {{ $mk->namaMK }}</th>
-                            @foreach ($list_cpl as $cpl)
-                            @php
-                                $isTrue = false;
-                                foreach ($list_cpmk->where('kodeCPL', $cpl->kodeCPL) as $cpmk) {
-                                    if ($detail_mk_cpmk->where('kodeMK', $mk->kodeMK)->where('kodeCPMK', $cpmk->kodeCPMK)->count()) {
-                                        $isTrue = true;
-                                        break;
-                                    }
-                                }
-                            @endphp
-                                <td>
-                                    <input type="checkbox" disabled @if ($isTrue) checked @endif>
-                                </td>
-                        @endforeach
-                        {{-- @foreach ($list_cpl as $cpl)
+                        @foreach ($list_cpl as $cpl)
                             @foreach ($list_cpmk->where('kodeCPL', $cpl->kodeCPL) as $cpmk)
                                 <td>
                                     <input type="checkbox" disabled @if ($detail_mk_cpmk->where('kodeMK', $mk->kodeMK)->where('kodeCPMK', $cpmk->kodeCPMK)->count()) checked @endif>
                                 </td>
                             @endforeach
-                        @endforeach --}}
+                        @endforeach
                     </tr>
                 @endforeach
             </tbody>
@@ -78,90 +64,48 @@
 
     </div>
     <style>
-
+    
         /* Style the tooltip */
-        span[itemid] {
-            position: relative;
-            cursor: pointer;
-            /* display: inline-block; */
-        }
-        span[itemid]:hover::after {
-            content: attr(itemid);
-            /* overflow: hidden; */
-            /* text-overflow: ellipsis; */
-            word-wrap: break-word;
-            /* membuat kata wrap ketika teks melebihi lebar elemen */
-            word-break: break-all;
-            /* memaksa kata dipisahkan ketika melebihi lebar elemen */
-            width: 350px;
-            height: auto;
-            background-color: #1F2F4D;
-            color: white;
-            padding: 9px;
-            border-radius: 5px;
-            position: absolute;
-            top: 35px;
-            /* bottom: -100px; */
-            /* left: 2%; */
-            transform: translateX(-88%);
-            /* white-space: nowrap; */
-            z-index: 1;
-            opacity: 1;
-            /* transition: opacity 3s; */
-            /* transition: opacity 0.3s ease, visibility 0s linear 0.3s; */
-        }
-        span[itemid]:hover::before {
-            content: "";
-            border-style: solid;
-            border-width: 0 8px 8px 8px;
-            border-color: transparent transparent #1F2F4D transparent;
-            position: absolute;
-            top: 28px;
-            left: 50%;
-            transform: translateX(-50%);
-            bottom: calc(100% + 10px);
-            /* transition: opacity 0.3s ease, visibility 0s linear 0.3s; */
-        }
-
-
-
-        input[type="checkbox"] {
-            -webkit-appearance: none;
-            -moz-appearance: none;
-            appearance: none;
-            border: 2px solid #ffffff;
-            border-radius: 1px;
-            width: 23px;
-            height: 25px;
-            outline: none;
-            cursor: pointer;
-            position: relative ;
-        }
-        input[type="checkbox"]:checked::before {
-            content: "\2714";
-            font-size: 14px;
-            color: white;
-            position: absolute;
-            /* left: 2px; */
-            /* top: 1px; */
+        /* Style the tooltip */
+         span[itemid] {
+          position: relative;
+          cursor: pointer;
+          /* display: inline-block; */
         }
         
-        input[type="checkbox"]:checked {
-            background-color: white;
-            border-color: #686565;
+        span[itemid]:hover::after {
+          content: attr(itemid);
+          overflow: hidden;
+          text-overflow: ellipsis;
+          /* width: 200px; */
+          background-color: #1F2F4D;
+          color: white;
+          padding: 9px;
+          border-radius: 5px;
+          position: absolute;
+          bottom: -40px;
+          left: 120%;
+          transform: translateX(-50%);
+          white-space: nowrap;
+          z-index: 1;
+          opacity: 1;
+          /* transition: opacity 3s; */
+          /* transition: opacity 0.3s ease, visibility 0s linear 0.3s; */
+        
+        }
+        
+        span[itemid]:hover::before {
+          content: "";
+          border-style: solid;
+          border-width: 0 8px 8px 8px;
+          border-color: transparent transparent #1F2F4D transparent;
+          position: absolute;
+          top: 12px;
+          left: 80%;
+          transform: translateX(-50%);
+          bottom: calc(100% + 10px);
+          /* transition: opacity 0.3s ease, visibility 0s linear 0.3s; */
         }
         
         </style>
-        <script>
-        let getIdCheckbox = document.getElementById("color")
-        changeColor = () => {
-            if (getIdCheckbox.checked == true){
-                document.getElementById("red").style.color = 'red'
-                document.getElementById("green").style.color = 'green'
-            }else {
-                document.getElementById("red").style.color = 'white'
-                document.getElementById("green").style.color = 'white'
-            }
-        }   
-    </script>
 @endsection
