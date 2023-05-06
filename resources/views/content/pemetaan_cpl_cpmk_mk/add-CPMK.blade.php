720c93a07813ed6d655803d87e0afffd3ed2a0b5
oxysh <oxy.s.hapsari04@gmail.com>
2023-05-06 18:45:37 +0700

    Revert "update branch kelompok2"
    This reverts commit 14db30537cb6f967cdc88e497d25d9105bcdc16c, reversing
    changes made to eadfb4e2577080435d0d61ac4562d0f93ecb1566.

--- resources/views/content/pemetaan_cpl_cpmk_mk/add-CPMK.blade.php
+++ resources/views/content/pemetaan_cpl_cpmk_mk/add-CPMK.blade.php
@@ -2,22 +2,10 @@
 
 @section('content')
     <form class="content px-4 pt-3" action="{{ route('kurikulum.pemetaan.cpl_cpmk_mk.cpmk.store') }}" method="POST">
-        @if ($errors->has('kodeCPMK') && $errors->has('deskripsiCPMK'))
-            <div class="card border" style="background-color: #eda5ac">
-                <div class="card-body" style="font-weight:600;">
-                    Kode CPMK dan Deskripsi CPMK harus diisi!
-                </div>
-            </div>
-        @elseif ($errors->has('deskripsiCPMK'))
+        @if ($errors->has('deskripsiCPMK'))
             <div class="card border" style="background-color: #eda5ac">
                 <div class="card-body" style="font-weight:600;">
                     Deskripsi CPMK harus diisi!
-                </div>
-            </div>
-        @elseif ($errors->has('kodeCPMK'))
-            <div class="card border" style="background-color: #eda5ac">
-                <div class="card-body" style="font-weight:600;">
-                    Kode CPMK harus diisi!
                 </div>
             </div>
         @endif
@@ -47,20 +35,13 @@
                         value="{{ $cpl->kodeCPL }}">
                 </div>
             </div>
-            <div class="row">
-                <div class="form-group col-2">
-                    <label for="kode-1">Kode CPMK</label>
-                    <input type="text" class="form-control" name="kodeCPMK" id="kode-1">
-                </div>
-                <div class="form-group col-10">
-                    <label for="deskripsi-1">Deskripsi CPMK</label>
-                    <textarea class="form-control" name="deskripsiCPMK" id="deskripsi-1" rows="3"></textarea>
-                </div>
+            <div class="form-group">
+                <label for="deskripsi-{{ $nextID }}">Deskripsi CPMK (Kode : {{ $nextID }})</label>
+                <textarea class="form-control" name="deskripsiCPMK" id="deskripsi-{{ $nextID }}" rows="3"></textarea>
             </div>
         </div>
         <textarea class="visually-hidden" name="deskripsi"></textarea>
-        <textarea class="visually-hidden" name="kode"></textarea>
-        <button type="button" class="btn btn-secondary" id="addField" data-lastid="1">Tambah field CPMK</button>
+        <button type="button" class="btn btn-secondary" id="addField" data-lastid="{{ $nextID }}">Tambah field CPMK</button>
         <button type="submit" class="btn btn-primary" id="submitCPMK">Kirim</button>
     </form>
 
@@ -89,40 +70,21 @@
     <script>
         // handle dynamic field CPMK
         $('#addField').click(e => {
-            newID = parseInt(e.currentTarget.dataset.lastid) + 1;
+            numID = parseInt(e.currentTarget.dataset.lastid.substring(4)) + 1;
+            newID = "CPMK" + String(numID).padStart(3, '0');
             e.currentTarget.dataset.lastid = newID;
-
-            label = document.createElement('label');
-            label.setAttribute('for', 'kode-' + newID);
-            label.innerText = 'Kode CPMK';
-            input = document.createElement('input');
-            input.setAttribute('type', 'text');
-            input.setAttribute('class', 'form-control');
-            input.setAttribute('name', 'kodeCPMK');
-            input.setAttribute('id', 'kode-' + newID);
-            input.setAttribute('rows', '3');
-            codeField = document.createElement('div');
-            codeField.setAttribute('class', 'form-group col-2');
-            codeField.append(label);
-            codeField.append(input);
-
             label = document.createElement('label');
             label.setAttribute('for', 'deskripsi-' + newID);
-            label.innerText = 'Deskripsi CPMK';
+            label.innerText = 'Deskripsi CPMK (Kode : ' + newID + ')';
             textarea = document.createElement('textarea');
             textarea.setAttribute('class', 'form-control');
             textarea.setAttribute('name', 'deskripsiCPMK');
             textarea.setAttribute('id', 'deskripsi-' + newID);
             textarea.setAttribute('rows', '3');
-            descField = document.createElement('div');
-            descField.setAttribute('class', 'form-group col-10');
-            descField.append(label);
-            descField.append(textarea);
-
             newField = document.createElement('div');
-            newField.setAttribute('class', 'row');
-            newField.append(codeField);
-            newField.append(descField);
+            newField.setAttribute('class', 'form-group');
+            newField.append(label);
+            newField.append(textarea);
             $('.form-input').append(newField);
         })
 
@@ -131,16 +93,12 @@
             e.preventDefault();
 
             let deskripsi = [];
-            let kode = [];
             $('textarea[name="deskripsiCPMK"]').each(x => {
-                code = $('input[name="kodeCPMK"]')[x].value;
-                kode.push('"' + code + '"')
                 desc = $('textarea[name="deskripsiCPMK"]')[x].value;
                 deskripsi.push('"' + desc + '"')
             })
             deskripsi = deskripsi.join();
             $('.visually-hidden[name="deskripsi"]')[0].innerText = deskripsi;
-            $('.visually-hidden[name="kode"]')[0].innerText = kode;
 
             $('form.content')[0].submit();
         })
