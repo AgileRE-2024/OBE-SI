d9da25bc699dc2f19db9b156b4ab4b6373978eb5
MalvinMM <juniormalvinn@gmail.com>
2023-05-24 02:35:43 +0700

    export pemetaan cpl-bk

--- resources/views/content/pemetaan_cpl_bk/exportpdf_cplbk.blade.php
+++ resources/views/content/pemetaan_cpl_bk/exportpdf_cplbk.blade.php
@@ -1,68 +1,67 @@
 <!DOCTYPE html>
 <html>
-    <head>
+
+<head>
     <title>{{ $judul }}</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <style>
         table {
             border-collapse: collapse;
             width: 100%;
         }
-        th, td {
+
+        th,
+        td {
             text-align: center;
             font-size: 10px;
             border: 1px solid black;
             padding: 5px;
         }
     </style>
-    </head>
-    <body>
-    <div class="container-fluid">
-        <div class="card border" style="background-color: white">
-            <div class="card-body" style="font-weight:600;">
-                <center>Pemetaan Capaian Pembelajaran Lulusan (CPL) & Bahan Kadjian (BK)</center>
-            </div>
-        </div>
-        <div class="table-responsive ">
-            <table class="table table-bordered" style="text-align: center">
-                <thead>
-                    <tr>
-                    <th class="align-middle" scope="col" rowspan="2" style="width: 5%">No</th>
-                    <th class="align-middle" scope="col" rowspan="2" style="width: 10%">Kode BK</th>
-                    <th class="align-middle" scope="col" rowspan="2" style="width: 50%">Nama Bahan Kajian</th>
-                    <th scope="col" colspan="{{ $list_cpl->count() }}">Capaian Pembelajaran Lulusan</th>
-                    </tr>
-                    <tr>
-                        @foreach ($list_cpl as $cpl)
+</head>
+
+<body>
+    <div>
+        <table style="text-align: center; border: 1px inherit black solid;width:100%">
+            <thead style="background-color: black; color: white">
+                <tr>
+                    <th scope="col" rowspan="2" style="width: 5%">No</th>
+                    <th scope="col" rowspan="2" style="width: 10%">Kode BK</th>
+                    <th scope="col" rowspan="2" style="width: 50%">Nama BK</th>
+                    <th scope="col" colspan="{{ $list_cpl->count() }}">Bahan Kajian (CPL)</th>
+                </tr>
+                <tr>
+                    @foreach ($list_cpl as $cpl)
                         <th scope="col">
-                            <span itemid="{{ $cpl->deskripsiCPL }}">
-                                {{ $cpl->kodeCPL }}
-                            </span>
-                            {{-- <span data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                title="{{ $cpl->deskripsiCPL }}">{{ $cpl->kodeCPL }}</span> --}}
-                            </th>
+                            <span data-bs-toggle="tooltip" data-bs-placement="bottom">{{ $cpl->kodeCPL }}</span>
+                        </th>
                     @endforeach
-                </thead>
-                <tbody>
-                    @foreach ($list_bk as $bk)
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($list_bk as $bk)
                     <tr>
-                        <th scope="row">
+                        <th scope="row" @if ($detail_cpl_bk->where('kodeBK', '===', $bk->kodeBK)->count() == 0) style="background-color: yellow;" @endif>
                             {{ $loop->iteration }}</th>
-                        <th scope="row">
+                        <th scope="row" @if ($detail_cpl_bk->where('kodeBK', '===', $bk->kodeBK)->count() == 0) style="background-color: yellow;" @endif>
                             {{ $bk->kodeBK }}</th>
-                        <th scope="row" class="text-start">
+                        <th scope="row"
+                            style="text-align: left; @if ($detail_cpl_bk->where('kodeBK', '===', $bk->kodeBK)->count() == 0) background-color: yellow; @endif">
                             {{ $bk->namaBK }}</th>
                         @foreach ($list_cpl as $cpl)
-                                <td>
-                                    <input type="checkbox" disabled @if ($detail_cpl_bk->where('kodeBK', $bk->kodeBK)->count()) checked @endif>
-                                </td>
+                            <td @if (
+                                $detail_cpl_bk->where('kodeCPL', '===', $cpl->kodeCPL)->count() == 0 ||
+                                    $detail_cpl_bk->where('kodeBK', '===', $bk->kodeBK)->count() == 0) style="background-color: yellow;" @endif>
+                                <input type="checkbox" style="width: 25px; height: 25px; border: none; outline: none;"
+                                    @if ($detail_cpl_bk->where('kodeBK', '===', $bk->kodeBK)->where('kodeCPL', '===', $cpl->kodeCPL)->count()) checked @endif>
+                            </td>
                         @endforeach
                     </tr>
                 @endforeach
-                </tbody>
-            </table>
-        </div>
-        </div>
+            </tbody>
+        </table>
     </div>
-    </body>
+
+</body>
+
 </html>
