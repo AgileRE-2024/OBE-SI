08f2d13d84f0a8fbb814463491d2de2b589bf557
oktavintfd <oktavia150908@gmail.com>
2023-11-22 15:24:48 +0700

    nambah atribut bobot nilai di rps

--- database/migrations/2023_04_26_154909_create_Minggu_RPS_table.php
+++ database/migrations/2023_04_26_154909_create_Minggu_RPS_table.php
@@ -30,6 +30,7 @@ return new class extends Migration
             $table->text('waktuPembelajaran')->nullable();
             $table->text('pengalaman_belajar')->nullable();
             $table->text('bahan_kajian')->nullable();
+            $table->string('bobot_nilai', 100)->nullable();
             $table->timestamps();
         });
     }
