4281f862a61c43cb67fae59ba2dbd02435845e6a
muhammad segaf <msegafhabsyi@gmail.com>
2023-11-22 18:19:20 +0700

    punya ilham

--- app/Http/Controllers/RPSController.php
+++ app/Http/Controllers/RPSController.php
@@ -191,7 +191,17 @@ class RPSController extends Controller
 
         return view('content.cari_rps', [
             'title' => 'RPS',
+            'rps_list'=> RPS::all(),
+            'teknik_penilaian_list'=> Teknik_Penilaian::all(),
+            'detail_rps_list'=> Detail_RPS::all(),
+            'dosen_list'=> User::all(),
+            'mk_list' => Mata_Kuliah::all(),
+            'minggu_rps_list' => Minggu_RPS::all(),
+            'detail_peran_dosen_list' => Detail_Peran_Dosen::all(),
+            'subcpmk_list'=>SubCPMK::all(),
+            'teknik_penilaian_list'=>Teknik_Penilaian::all(),
             'rps' => $rps,
+
         ]);
     }
 
