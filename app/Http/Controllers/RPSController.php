c1dd0153e3634a6e753369d77e19d97d0e801d9f
oktavintfd <oktavia150908@gmail.com>
2023-12-05 10:42:56 +0700

    filter sub cpmk matkul itu aja

--- app/Http/Controllers/RPSController.php
+++ app/Http/Controllers/RPSController.php
@@ -217,13 +217,11 @@ class RPSController extends Controller
     // NEW FUNCTION 3
     public function detail($kodeRPS){
         $kodeMK = substr($kodeRPS, 0, 6);
-        $cpmk_mk = Detail_MK_CPMK::all()->where('kodeMK', $kodeMK);
-        $kodeCPMKList = $cpmk_mk->pluck('kodeCPMK')->toArray();
+        $kodeCPMKList = Detail_MK_CPMK::all()->where('kodeMK', $kodeMK)->pluck('kodeCPMK')->toArray();
         $cpmk = CPMK::whereIn('kodeCPMK', $kodeCPMKList)->get();
         $kodeCPLList = $cpmk->pluck('kodeCPL')->toArray();
         $semuaCPL = CPL_Prodi::whereIn('kodeCPL', $kodeCPLList)->distinct()->get();
-        $prasyarat = Prasyarat::all()->where('kodeMK', $kodeMK);
-        $kodePrasyaratList = $prasyarat->pluck('mat_kodeMK')->toArray();
+        $kodePrasyaratList = Prasyarat::all()->where('kodeMK', $kodeMK)->pluck('mat_kodeMK')->toArray();
         $prasyarat = Mata_Kuliah::whereIn('kodeMK', $kodePrasyaratList)->distinct()->get();
 
         return view('rps_mata_kuliah', [
