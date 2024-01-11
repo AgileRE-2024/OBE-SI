c4658e4c044bf01c0065f3ea8e7a2aaf67da1b34
dtrzka <102124897+dtrzka@users.noreply.github.com>
2024-01-11 12:23:54 +0700

    Update DetailMkCpmkSeeder.php

--- database/seeders/DetailMkCpmkSeeder.php
+++ database/seeders/DetailMkCpmkSeeder.php
@@ -46,6 +46,246 @@ class DetailMkCpmkSeeder extends Seeder
             'kodeCPMK' => 'CPMK006',
         ]);
 
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'NOP103',
+            'kodeCPMK' => 'CPMK007',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'NOP101',
+            'kodeCPMK' => 'CPMK008',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'KID107',
+            'kodeCPMK' => 'CPMK009',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'BID108',
+            'kodeCPMK' => 'CPMK010',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MAA101',
+            'kodeCPMK' => 'CPMK011',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'BAI101',
+            'kodeCPMK' => 'CPMK012',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIP107',
+            'kodeCPMK' => 'CPMK013',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SII103',
+            'kodeCPMK' => 'CPMK014',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'FID112',
+            'kodeCPMK' => 'CPMK015',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MAL103',
+            'kodeCPMK' => 'CPMK016',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MAT101',
+            'kodeCPMK' => 'CPMK017',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MAT102',
+            'kodeCPMK' => 'CPMK018',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MNM107',
+            'kodeCPMK' => 'CPMK019',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MNM106',
+            'kodeCPMK' => 'CPMK020',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MAS118',
+            'kodeCPMK' => 'CPMK021',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'BAE111',
+            'kodeCPMK' => 'CPMK022',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIA204',
+            'kodeCPMK' => 'CPMK023',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIA205',
+            'kodeCPMK' => 'CPMK024',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SID201',
+            'kodeCPMK' => 'CPMK025',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SID203',
+            'kodeCPMK' => 'CPMK026',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIR201',
+            'kodeCPMK' => 'CPMK027',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIR203',
+            'kodeCPMK' => 'CPMK028',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MAS234',
+            'kodeCPMK' => 'CPMK029',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIJ201',
+            'kodeCPMK' => 'CPMK030',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIJ203',
+            'kodeCPMK' => 'CPMK031',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SII301',
+            'kodeCPMK' => 'CPMK032',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SII314',
+            'kodeCPMK' => 'CPMK032',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIC201',
+            'kodeCPMK' => 'CPMK033',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIC202',
+            'kodeCPMK' => 'CPMK034',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIJ202',
+            'kodeCPMK' => 'CPMK035',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIJ204',
+            'kodeCPMK' => 'CPMK035',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIS202',
+            'kodeCPMK' => 'CPMK036',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MNM203',
+            'kodeCPMK' => 'CPMK037',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIS304',
+            'kodeCPMK' => 'CPMK038',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIS305',
+            'kodeCPMK' => 'CPMK038',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIS205',
+            'kodeCPMK' => 'CPMK039',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SII213',
+            'kodeCPMK' => 'CPMK040',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'BAE112',
+            'kodeCPMK' => 'CPMK041',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIS309',
+            'kodeCPMK' => 'CPMK042',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'MNW401',
+            'kodeCPMK' => 'CPMK043',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SII208',
+            'kodeCPMK' => 'CPMK044',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SII209',
+            'kodeCPMK' => 'CPMK045',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SII318',
+            'kodeCPMK' => 'CPMK046',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SII319',
+            'kodeCPMK' => 'CPMK046',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SID303',
+            'kodeCPMK' => 'CPMK047',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SID304',
+            'kodeCPMK' => 'CPMK048',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIA301',
+            'kodeCPMK' => 'CPMK049',
+        ]);
+
+        Detail_MK_CPMK::create([
+            'kodeMK' => 'SIA302',
+            'kodeCPMK' => 'CPMK049',
+        ]);
+
         // Detail_MK_CPMK::create([
         //     'kodeMK' => 'SII103',
         //     'kodeCPMK' => 'CPMK011',
