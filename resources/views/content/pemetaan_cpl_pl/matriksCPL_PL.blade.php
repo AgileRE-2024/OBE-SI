e19bb6d5b50aab3fb2574cfb955911d11ac3eda4
Oktavia Intifada <oktavia150908@gmail.com>
2024-03-27 09:58:51 +0700

    resolve mapping cpl pl edit access

--- resources/views/content/pemetaan_cpl_pl/matriksCPL_PL.blade.php
+++ resources/views/content/pemetaan_cpl_pl/matriksCPL_PL.blade.php
@@ -67,7 +67,7 @@
                                         name="checkbox_{{ $cpl->kodeCPL }}-{{ $pl->kodePL }}"
                                         value="{{ $cpl->kodeCPL }}&{{ $pl->kodePL }}"
                                         @if ($pemetaan->where('kodeCPL', '===', $cpl->kodeCPL)->where('kodePL', '===', $pl->kodePL)->count()) checked @endif
-                                        @if (auth()->user()->role != 1) disabled @endif>
+                                        @if (auth()->user()->role == 0 || auth()->user()->role == 2) disabled @endif>
                                 </td>
                             @endforeach
                         </tr>
