7fe773320a507f1b109da1aad8d8b6f51958e0db
Oktavia Intifada <oktavia150908@gmail.com>
2024-05-03 11:05:17 +0700

    update migration minggu rps

--- database/migrations/2024_05_03_031001_add_id_instrumen_penilaian_to_minggurps.php
+++ database/migrations/2024_05_03_031001_add_id_instrumen_penilaian_to_minggurps.php
@@ -13,15 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('minggu_rps', function (Blueprint $table) {
-            Schema::table('minggu_rps', function (Blueprint $table) {
+        Schema::table('Minggu_RPS', function (Blueprint $table) {
                 $table->unsignedInteger('id_instrumen_penilaian')->nullable();
                 $table->unsignedInteger('id_teknik_penilaian')->nullable();
                 $table->unsignedBigInteger('id_komponen_penilaian')->nullable();
                 $table->foreign('id_komponen_penilaian')->references('id_komponen_penilaian')->on('komponen_penilaian')->onDelete('restrict');
                 $table->foreign('id_teknik_penilaian')->references('id_teknik_penilaian')->on('teknik_penilaian_rps')->onDelete('restrict');
                 $table->foreign('id_instrumen_penilaian')->references('id_instrumen_penilaian')->on('instrumen_penilaian')->onDelete('restrict');
-            });
         });
     }
 
